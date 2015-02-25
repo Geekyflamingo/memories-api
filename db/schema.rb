395e349fa1ecd85f4ee412193332b15b6e07ee6e
Brett Chalupa <brettchalupa@gmail.com>
2015-02-24 19:03:16 -0500

    Add timestamps to the moments model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224214241) do
+ActiveRecord::Schema.define(version: 20150224221721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20150224214241) do
     t.text     "caption"
     t.datetime "happened_at"
     t.string   "location"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
